e686d86d72404ee9e9a3616a821b283fe4c096bd
1PasswordSDKBot <it-github-sdk-bot@agilebits.com>
2024-05-02 20:22:51 +0000

    Update core to version 1febdf3e

--- src/onepassword/secrets.py
+++ src/onepassword/secrets.py
@@ -3,13 +3,10 @@ from json import loads
 
 
 class Secrets:
-    """Contains all operations the SDK client can perform on 1Password secrets."""
-
     def __init__(self, client_id):
         self.client_id = client_id
 
     async def resolve(self, secret_reference):
-        """Resolve the secret reference to a secret."""
         response = await _invoke(
             {
                 "clientId": self.client_id,
@@ -21,4 +18,5 @@ class Secrets:
                 },
             }
         )
-        return loads(response)
+        result = str(loads(response))
+        return result
