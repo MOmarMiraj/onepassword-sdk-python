6cb1796f37a579285490c1a4fe4dab06f1ce1788
Omar Miraj <omar.miraj@agilebits.com>
2024-06-11 13:51:30 -0400

    Update to two step release process

--- Makefile
+++ Makefile
@@ -1,10 +1,6 @@
-
-check_if_gh_is_installed:
-	@if ! command -v gh &> /dev/null; then\
-		echo "gh is not installed";\
-		exit 1;\
-	fi
-
 release:
-	make check_if_gh_is_installed
-	scripts/python_release.sh
+	scripts/release.sh
+
+prep-release:
+	scripts/prep-release.sh
+	
--- src/onepassword/defaults.py
+++ src/onepassword/defaults.py
@@ -1,12 +1,11 @@
 import platform
 
 SDK_LANGUAGE = "Python"
-SDK_VERSION =  open("../src/onepassword/version-build.txt").read()
+SDK_VERSION =  open("../onepassword-sdk-python/src/onepassword/version-build").read()
 DEFAULT_INTEGRATION_NAME = "Unknown"
 DEFAULT_INTEGRATION_VERSION = "Unknown"
 DEFAULT_REQUEST_LIBRARY = "net/http"
 DEFAULT_OS_VERSION = "0.0.0"
-
 
 # Generates a configuration dictionary with the user's parameters
 def new_default_config(auth, integration_name, integration_version):
