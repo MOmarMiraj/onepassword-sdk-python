a48098ce4cea280112be1fb6e3a6e256487d8686
Omar Miraj <omar.miraj@agilebits.com>
2024-06-14 10:52:13 -0400

    Add latest changes

---
+++ src/release/scripts/release.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Helper script to release the Python SDK
+
+set -e
+
+# Read the contents of the files into variables
+changelog=$(<src/release/changelogs/"${version}"-"${build}")
+version=$(awk -F "['\"]" '/SDK_VERSION =/{print $2}' "src/release/version.py")
+build=$(awk -F "['\"]" '/SDK_BUILD_NUMBER =/{print $2}' "src/release/version.py")
+echo $version
+echo $build
+
+# Check if Github CLI is installed
+if ! command -v gh &> /dev/null; then
+	echo "gh is not installed";\
+	exit 1;\
+fi
+
+# Ensure GITHUB_TOKEN env var is set
+if [ -z "${GITHUB_TOKEN}" ]; then
+  echo "GITHUB_TOKEN environment variable is not set."
+  exit 1
+fi
+
+git tag -a -s  "v${version}" -m "${version}"
+
+# Add changes and commit/push to branch
+git add .
+git commit -S -m "Release v${version}"
+
+git push --set-upstream origin "${branch}"
+
+gh release create "v${version}" --title "Release ${version}" --notes "${changelog}" --repo github.com/MOmarMiraj/onepassword-sdk-python
+
