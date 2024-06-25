3f82139f65abdbd491c41a97f15d97967608cd48
Omar Miraj <omar.miraj@agilebits.com>
2024-06-25 09:21:40 -0400

    Change name to release notes

--- src/release/scripts/prep-release.sh
+++ src/release/scripts/prep-release.sh
@@ -87,11 +87,9 @@ update_and_validate_build
 # Update version & build number in version.py
 sed -e "s/{{ build }}/$build/" -e "s/{{ version }}/$version/" "$version_template_file" > "$output_version_file"
 
-changelog_file="src/release/CHANGELOG"
-
 printf "Press ENTER to edit the CHANGELOG in your default editor...\n"
 read -r _ignore
-${EDITOR:-nano} "$changelog_file"
+${EDITOR:-nano} "src/release/CHANGELOG"
 
 # Get Current Branch Name
 branch="$(git rev-parse --abbrev-ref HEAD)"
