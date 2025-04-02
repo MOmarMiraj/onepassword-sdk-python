309650a32e12c5450865b585027c58d3eb9399ad
Omar Miraj <omar.miraj@agilebits.com>
2025-04-02 16:31:55 -0400

    fix setup and core.py to correctly include the new libs

--- src/onepassword/core.py
+++ src/onepassword/core.py
@@ -9,24 +9,29 @@ from onepassword.errors import raise_typed_exception
 MESSAGE_LIMIT = 50 * 1024 * 1024
 
 machine_arch = platform.machine().lower()
-mac_verison = platform.mac_ver()[0].split(".")[0] # gets the main version of macOS
+python_version = platform.python_version()[:4]
 base_path = "onepassword.lib"
-os = platform.system().lower()
-if os == "darwin":
-    python_version = platform.python_version()[:4]
-    if machine_arch in ["x86_64", "amd64"]:
-        if python_version > "3.13":
-            import onepassword
-        else
-            onepassword.lib.x86_64-macsox-10.9.op_uniffi_core as core
-# if machine_arch in ["x86_64", "amd64"]:
-#     import onepassword.lib.x86_64.op_uniffi_core as core
-# elif machine_arch in ["aarch64", "arm64"]:
-#     import onepassword.lib.aarch64.op_uniffi_core as core
-# else:
-#     raise ImportError(
-#         f"Your machine's architecture is not currently supported: {machine_arch}"
-#     )
+
+arch_map = {
+    "x86_64": "x86_64",
+    "amd64": "x86_64",
+    "aarch64": "aarch64",
+    "arm64": "aarch64",
+}
+# import onepassword.lib.aarch64_macosx_11_0.op_uniffi_core as core
+
+if machine_arch not in arch_map:
+    raise ImportError(f"Your machine's architecture is not currently supported: {machine_arch}")
+
+if platform.system().lower() == "darwin":
+    if arch_map[machine_arch] == "x86_64":
+        core_path = f"{base_path}.x86_64_macosx_10_13.op_uniffi_core" if python_version > "3.13" else f"{base_path}.x86_64_macosx_10_9.op_uniffi_core"
+    else:
+        core_path = f"{base_path}.aarch64_macosx_11_0.op_uniffi_core"
+else:
+    core_path = f"{base_path}.{arch_map[machine_arch]}.op_uniffi_core"
+
+core = __import__(core_path, fromlist=["core"])
 
 
 # InitClient creates a client instance in the current core module and returns its unique ID.
