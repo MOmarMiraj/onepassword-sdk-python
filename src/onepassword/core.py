5aada8d2aa3cf584ad21a944570cd865e858b426
Omar Miraj <omar.miraj@agilebits.com>
2025-04-02 23:54:19 -0400

    go bacck to old changes

--- src/onepassword/core.py
+++ src/onepassword/core.py
@@ -8,40 +8,29 @@ from onepassword.errors import raise_typed_exception
 # We set the limit to 50MB to be safe and consistent with the other SDKs (where this limit is 64MB), to be reconsidered upon further testing
 MESSAGE_LIMIT = 50 * 1024 * 1024
 
-machine_arch = platform.machine().lower()
+machine_arch = platform.machine()
+python_version = platform.python_version()[:4]
+base_path = "onepassword.lib"
 
-if machine_arch in ["x86_64", "amd64"]:
-    import onepassword.lib.x86_64.op_uniffi_core as core
-elif machine_arch in ["aarch64", "arm64"]:
-    import onepassword.lib.aarch64.op_uniffi_core as core
+arch_map = {
+    "x86_64": "x86_64",
+    "amd64": "x86_64",
+    "aarch64": "aarch64",
+    "arm64": "aarch64",
+}
+
+if machine_arch not in arch_map:
+    raise ImportError(f"Your machine's architecture is not currently supported: {machine_arch}")
+
+if platform.system().lower() == "Darwin":
+    if arch_map[machine_arch] == "x86_64":
+        core_path = f"{base_path}.x86_64_macosx_10_13.op_uniffi_core" if python_version > "3.13" else f"{base_path}.x86_64_macosx_10_9.op_uniffi_core"
+    else:
+        core_path = f"{base_path}.aarch64_macosx_11_0.op_uniffi_core"
 else:
-    raise ImportError(
-        f"Your machine's architecture is not currently supported: {machine_arch}"
-    )
+    core_path = f"{base_path}.{arch_map[machine_arch]}.op_uniffi_core"
 
-# machine_arch = platform.machine()
-# python_version = platform.python_version()[:4]
-# base_path = "onepassword.lib"
-
-# arch_map = {
-#     "x86_64": "x86_64",
-#     "amd64": "x86_64",
-#     "aarch64": "aarch64",
-#     "arm64": "aarch64",
-# }
-
-# if machine_arch not in arch_map:
-#     raise ImportError(f"Your machine's architecture is not currently supported: {machine_arch}")
-
-# if platform.system().lower() == "Darwin":
-#     if arch_map[machine_arch] == "x86_64":
-#         core_path = f"{base_path}.x86_64_macosx_10_13.op_uniffi_core" if python_version > "3.13" else f"{base_path}.x86_64_macosx_10_9.op_uniffi_core"
-#     else:
-#         core_path = f"{base_path}.aarch64_macosx_11_0.op_uniffi_core"
-# else:
-#     core_path = f"{base_path}.{arch_map[machine_arch]}.op_uniffi_core"
-
-# core = __import__(core_path, fromlist=["core"])
+core = __import__(core_path, fromlist=["core"])
 
 
 # InitClient creates a client instance in the current core module and returns its unique ID.
